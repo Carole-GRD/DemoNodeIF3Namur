d01c7aa890025b7b2e58827dd4df007fb0c23415
Carole Gérard <gerard.carole@skynet.be>
2022-07-13 15:50:17 +0200

    CORRECTION : Suppression de la variable inutile dans le fichier app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const calcul = require('./Modules/Simple/log');
+
 const calculModule = require('./Modules/Simple/log');
 
-calcul.noel();
-calcul.anniversaire();
-calcul.vacances();
+calculModule.noel();
+calculModule.anniversaire();
+calculModule.vacances();
