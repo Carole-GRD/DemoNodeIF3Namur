9957a34af21f474cb43e849e8244e0d52d7e4f9a
Carole Gérard <gerard.carole@skynet.be>
2022-07-13 15:47:07 +0200

    Créer un module qui permet de calculer un nombre de jours.

---
+++ Modules/Simple/log.js
@@ -0,0 +1,44 @@
+// date actuelle
+let dateDuJour = new Date();
+
+// Noël
+let dateNoel = new Date(`December 25, 2022 00:00:00`);
+
+// anniversaire
+let dateAnniveraire = new Date('March 16, 1978 00:03:00');
+
+// vacances
+let dateVacances = new Date('October 01, 2022 00:00:00');
+
+// l'objet "calcul" reprenant les différentes fonctions à exporter
+let calcul = {
+    noel : function(){
+        let interval = (dateNoel - dateDuJour);
+        let nbJours = Math.floor(interval / (1000 * 60 * 60 * 24));
+        console.log(`Il reste ${nbJours} jours avant Noël.`);
+
+    },
+
+    anniversaire : function(){
+        let interval = (dateAnniveraire - dateDuJour);
+        let nbJours = Math.floor(interval / (1000 * 60 * 60 * 24));
+        if(nbJours < 0){
+            let reponse = nbJours * -1;
+            console.log(`Il reste ${reponse} jours avant mon anniversaire.`);
+        }
+        else{
+            console.log(`Il reste ${nbJours} jours avant mon anniversaire.`);
+        }
+
+    },
+
+    vacances : function(){
+        let interval = (dateVacances - dateDuJour);
+        let nbJours = Math.floor(interval / (1000 * 60 * 60 * 24));
+        console.log(`Il reste ${nbJours} jours avant les vacances.`);
+
+    }
+}
+
+
+module.exports = calcul;
---
+++ app.js
@@ -0,0 +1,6 @@
+const calcul = require('./Modules/Simple/log');
+const calculModule = require('./Modules/Simple/log');
+
+calcul.noel();
+calcul.anniversaire();
+calcul.vacances();
