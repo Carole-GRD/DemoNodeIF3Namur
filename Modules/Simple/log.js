cbae26921e7980f615ede14329c5c8caff281d8c
Carole Gérard <gerard.carole@skynet.be>
2022-07-13 17:53:21 +0200

    Ajout de la fonction 'anniversaire' dans le fichier log.js

--- Modules/Simple/log.js
+++ Modules/Simple/log.js
@@ -5,7 +5,7 @@ let dateDuJour = new Date();
 let dateNoel = new Date(`December 25, 2022 00:00:00`);
 
 // anniversaire
-let dateAnniveraire = new Date('March 16, 1978 00:03:00');
+let dateAnniveraire = new Date('March 16, 2022 00:03:00');
 
 // vacances
 let dateVacances = new Date('October 01, 2022 00:00:00');
@@ -20,8 +20,15 @@ let calcul = {
     },
 
     anniversaire : function(){
-        
-
+        let interval = (dateAnniveraire - dateDuJour);
+        let nbJours = Math.floor(interval / (1000 * 60 * 60 * 24));
+        if(nbJours < 0){
+            let reponse = 365 + nbJours;
+            console.log(`Il reste ${reponse} jours avant mon anniversaire.`);
+        }
+        else{
+            console.log(`Il reste ${nbJours} jours avant mon anniversaire.`);
+        }
     },
 
     vacances : function(){
