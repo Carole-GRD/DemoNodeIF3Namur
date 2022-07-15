b5acfb20bb86d53536495e9a2489a09755d19507
Carole Gérard <gerard.carole@skynet.be>
2022-07-15 21:14:14 +0200

    Ajout de commentaires (explications) dans le fichier 'logCorrection.js'.

--- Modules/Simple/logCorrection.js
+++ Modules/Simple/logCorrection.js
@@ -2,9 +2,18 @@
 // -------------  LES CONSTANTES ET LES FONCTIONS   ----------------
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// Nombre de millisecondes dans une journée
 const milliOneDay = (24*3600*1000);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Date du jour 
 const today = new Date();
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Fonction qui vérifie si la date de l'évènement (Noël, anniversaire,...) est déjà passée ou pas
+// Dans le cas où la date est déjà passée, on ajoute un an pour obtenir la date correpondant à l'évènement de l'année suivante
 function getNextDate(day, month){
     let currentYear = today.getFullYear();
     let dateMonth = month - 1;
@@ -16,7 +25,9 @@ function getNextDate(day, month){
     return new Date(currentYear, dateMonth, day)
 }
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// Fonction qui calcul le nombre de jour avant l'évènement
 function getDiffDays(targetDate){
     // On obtient la différence en millisecondes entre nos deux dates
     const diff = targetDate.getTime() - today.getTime();
@@ -56,12 +67,15 @@ let eventDate = {
     // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     solstice : function(){
+        // on vérifie si les dates sont déjà passées (dans ce cas, on passe au solstice de l'année suivante)
         const nextSummerSolstice = getNextDate(21, 6);
         const nextWinterSolstice = getNextDate(21, 12);
-        
+
+        // on obtient le nombre de jours avant les prochains solstices d'été et d'hiver
         const diffDaysSummer = getDiffDays(nextSummerSolstice);
         const diffDaysWinter = getDiffDays(nextWinterSolstice);
-    
+        
+        // Math.min nous renvoie le nombre le plus petit et donc le prochain solstice le plus proche de la date du jour
         console.log(`Il reste ${Math.min(diffDaysSummer, diffDaysWinter)} jours avant le prochain solstice.`);
     },
     
@@ -69,17 +83,21 @@ let eventDate = {
     
     fromFriday13 : function(){
         let month = today.getMonth();
-    
+        
+        // si le 13 du mois en cours est déjà passé, on passe au mois suivant pour vérifier si le 13 est un vendredi
         if (today.getDate() >= 13){
             month++;
         }
-    
+        
+        // on stocke la date du 13 du mois (en cours ou suivant) de l'année (en cours) dans une variable 
         const nextFriday = new Date(today.getFullYear(), month, 13);
-    
+        
+        // Tant que le 13 du mois n'est pas un vendredi (vendredi a pour index 5), on passe au mois suivant pour vérifier si le 13 est un vendredi
         while(nextFriday.getDay() !== 5){
             nextFriday.setMonth(nextFriday.getMonth() + 1);
         }
-    
+        
+        // lorsqu'on a la date du prochain vendredi 13, on calcul le nombre de jours avant cette date
         const diffDays = getDiffDays(nextFriday);
         console.log(`Il reste ${diffDays} avant le prochain vendredi 13`);
     }
